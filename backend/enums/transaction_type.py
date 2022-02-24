596aa0e51c6933014e1d795fcb9232249838bec5
Clarence Rhey Salaveria <clarencerheysalaveria@plv.edu.ph>
2022-02-24 23:52:55 +0800

    [IMPROVEMENT] (T-381) / Add docstring to transaction type

--- backend/enums/transaction_type.py
+++ backend/enums/transaction_type.py
@@ -4,9 +4,10 @@ from enum import IntEnum
 class TransactionType(IntEnum):
     """Enum to describe a transaction type
 
-    ORDER - A transaction from the mobile application
-    BUY - A transaction from the walk in customers
-    ADMIN - Transactions done by admins
+    ``Attributes:``
+        ORDER (int): A transaction from the mobile application
+        BUY (int): A transaction from the walk in customers
+        ADMIN (int): Transactions done by admins
     """
     ORDER = 0
     BUY = 1
