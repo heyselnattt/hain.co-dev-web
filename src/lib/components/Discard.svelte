f6d7664a9ed4e0b7c73abbf1bcef6c3a517e06e5
brmangulabnan <brmangulabnan.edu@gmail.com>
2022-03-11 00:11:41 +0800

    [ADD] (T-409) / Add components for Discard, Add record, and Back buttons

---
+++ src/lib/components/Discard.svelte
@@ -0,0 +1,17 @@
+<button class="btn-txt button is-danger is-rounded mt-3" href="/">
+    <img class="discard" src="delete.png" alt="back"/>
+    <p class="ml-2">
+        Discard
+    </p>
+</button>
+
+<style>
+    .discard {
+        width: 30px;
+    }
+
+    .btn-txt {
+        font-size: 20px;
+        font-family: 'Karla', sans-serif;
+    }
+</style>
