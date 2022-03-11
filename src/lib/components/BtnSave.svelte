1637012fdce25ab4c20c5757b1b5423cf677bdf0
Hazel Anne Mejias <hazelannemejias1@gmail.com>
2022-03-11 18:11:27 +0800

    [ADD] / Added Save Button

---
+++ src/lib/components/BtnSave.svelte
@@ -0,0 +1,12 @@
+<button class="button is-rounded is-info btn-txt">
+    <p class="ml-4 mr-4 has-text-white">
+        Save
+    </p>
+</button>
+
+<style>
+    .btn-txt {
+        font-size: 20px;
+        font-family: 'Karla', sans-serif;
+    }
+</style>
