31a550df16811cebf6bb3331e324c6ba401a9f32
Hazel Anne Mejias <hazelannemejias1@gmail.com>
2022-03-12 15:13:49 +0800

    [REFACTOR] / renamed components/routes

---
+++ src/lib/components/ButtonAddRecord.svelte
@@ -0,0 +1,17 @@
+<button class="btn-txt button is-link is-rounded mt-3" href="/">
+    <img class="img2" src="images/add.png" alt="back"/>
+    <p class="ml-2">
+        Add Record
+    </p>
+</button>
+
+<style>
+    .img2 {
+        width: 30px;
+    }
+
+    .btn-txt {
+        font-size: 20px;
+        font-family: 'Karla', sans-serif;
+    }
+</style>
