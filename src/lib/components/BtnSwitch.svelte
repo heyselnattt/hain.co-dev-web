38a885477266b7e28783ed3ed9b287cd712c359a
brmangulabnan <brmangulabnan.edu@gmail.com>
2022-03-12 21:50:46 +0800

    Added switch button as component

---
+++ src/lib/components/BtnSwitch.svelte
@@ -0,0 +1,8 @@
+<script>
+    
+</script>
+
+<div class="field">
+    <input id="switchLarge switchColorDefault switchRoundedDefault" type="checkbox" name="switchLarge switchColorDefault switchRoundedDefault" class="switch is-large is-link is-rounded">
+    <label for="switchLarge switchColorDefault switchRoundedDefault"></label>
+</div>
