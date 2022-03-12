403ad4dc97af735f27d307980a79efdf17a7d9b6
Hazel Anne Mejias <hazelannemejias1@gmail.com>
2022-03-12 15:14:50 +0800

    [ADD] (Close T-434) / Added reports page and icon

---
+++ src/lib/components/ReportsTableRow.svelte
@@ -0,0 +1,23 @@
+<script>
+    export let recordID;
+    export let dateRange;
+    export let recordType;
+</script>
+
+<tbody>
+    <tr class="text is-clickable" >
+        <th>{recordID}</th>
+        <td>{dateRange}</td>
+        <td>{recordType}</td>
+        <td><img src="images/downloadIcon.png" alt="download"></td>
+    </tr>
+</tbody>
+
+<style>
+    .text {
+        font-size: 17px;
+    }
+    img {
+        max-width: 25px;
+    }
+</style>
