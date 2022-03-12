53d8d3d42ac179144b154a4a9512d4b37e58244a
Hazel Anne Mejias <hazelannemejias1@gmail.com>
2022-03-12 15:13:49 +0800

    [REFACTOR] / renamed components/routes

--- src/routes/CanteenStaffInfo.svelte
+++ src/routes/CanteenStaffInfo.svelte
@@ -1,44 +1,42 @@
 <script>
-    import Discard from "$lib/components/Discard.svelte";
+    import ButtonBack from "$lib/components/ButtonBack.svelte";
+    import ButtonEdit from "$lib/components/ButtonEdit.svelte";
     import FieldWithValue from "$lib/components/FieldWithValue.svelte";
     import NavbarSolo from "$lib/components/NavbarSolo.svelte";
-    import ButtonBack from "$lib/components/ButtonBack.svelte";
-    import ButtonAddRecord from "$lib/components/ButtonAddRecord.svelte";
 </script>
 
-<NavbarSolo />
+<svelte:head>
+    <link href="https://fonts.googleapis.com/css2?family=Karla:wght@600&display=swap" rel="stylesheet"/>
+</svelte:head>
+
+<NavbarSolo/>
 
 <div class="container">
     <div class="columns  pt-5 is-multiline has-text-centered">
         <div class="column is-4"> 
-            <a href="Customers"> <ButtonBack /> </a> 
+            <a href="CanteenStaff"> <ButtonBack /> </a> 
         </div>
         <div class="column is-4">
             <p class="text has-text-link">
-                New Customer
+                Canteen Staff's Information
             </p>
         </div>
         <div class="column is-3 ml-6">
-            <a href="/"> <Discard /> </a>
+            <a href="EditCanteenStaffInfo"> <ButtonEdit /> </a>
         </div>
 
         <div class="column is-12"></div>
         <div class="column is-12"></div>
         <div class="column is-12"></div>
-        <FieldWithValue name="Name" value=""/>
-        <FieldWithValue name="Contact No." value=""/>
+        <FieldWithValue name="Name" value="Salaveria, Rence"/>
+        <FieldWithValue name="Position" value="Cook"/>
         <div class="column is-12"></div>
-        <FieldWithValue name="Email" value=""/>
-        <FieldWithValue name="Password" value=""/>
-        <div class="column is-12"></div>
-        <div class="column is-12"></div>
+        <FieldWithValue name="Contact No." value="09123456789"/>
+        <FieldWithValue name="Address" value="02 Toronto St. Malinta Valenzuela City"/>
         <div class="column is-12"></div>
         <div class="column is-12"></div>
-    </div>
-
-    <!-- Add record button -->
-    <div class="mb- has-text-centered">
-        <a href="Customers"> <ButtonAddRecord /> </a>
+        <div class="column is-12"></div>
+        <div class="column is-12"></div>
     </div>
 </div>
 
