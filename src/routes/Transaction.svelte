086a3fb73fea6fbd96faacfba5e8a7d9c5e8af47
Gemarie Magtangob <magtangobgemarie@gmail.com>
2022-03-13 14:19:04 +0800

    [ADD] (T-432) / Added component/routes transaction

---
+++ src/routes/Transaction.svelte
@@ -0,0 +1,59 @@
+<script>
+    import NavbarWithSearch from "$lib/components/NavbarWithSearch.svelte";
+    import BtnBack from "$lib/components/BtnBack.svelte";
+    import TransactionTableRow from "$lib/components/TransactionTableRow.svelte";
+</script>
+
+<svelte:head>
+    <link href="https://fonts.googleapis.com/css2?family=Karla:wght@600&display=swap" rel="stylesheet"/>
+</svelte:head>
+
+<NavbarWithSearch />
+
+<div class="container">
+    <div class="columns has-text-centered pt-5">
+        <div class="column is-4 has-text-centered">
+            <a href="Database"><BtnBack /></a>
+        </div>
+        <div class="column is-4">
+            <p class="text has-text-link">
+                Transactions
+            </p>
+        </div>
+    </div>
+
+    <div class="column is-10 is-offset-1 pl-5 pt-0">
+        <table class="table is-hoverable is-fullwidth">
+            <thead>
+                <tr>
+                    <th>Transaction ID</th>
+                    <th>Name</th>
+                    <th>Description</th>
+                    <th>Amount</th>
+                    <th>Type</th>
+                </tr>
+            </thead>
+            <TransactionTableRow num="0001" name="Bantugan, Dea" desc="buy rice and adobo" amount="40.00" type="Order"/>
+            <TransactionTableRow num="0002" name="Beltran, Sheng" desc="buy RC" amount="10.00" type="Buy"/>
+            <TransactionTableRow num="0003" name="Albis, Anne" desc="buy sopas" amount="20.00" type="Order"/>
+            <TransactionTableRow num="0004" name="Perry, Katy" desc="buy royal, rice, hotdog" amount="50.00" type="Order"/>
+            <TransactionTableRow num="0005" name="Mendes Shawn" desc="buy clover, buy RC" amount="18.00" type="Order"/>
+            <TransactionTableRow num="0006" name="Cabello, Camilla" desc="buy pork steak" amount="40.00" type="Order"/>
+            <TransactionTableRow num="0007" name="Gomez, Selena" desc="edit the student's info" amount="----" type="Admin"/>
+            <TransactionTableRow num="0008" name="Mendoza, Maine" desc="buy 5 RC" amount="50.00" type="Buy"/>
+            <TransactionTableRow num="0009" name="Kim, Sunwoo" desc="buy rice" amount="15.00" type="Buy"/>
+        </table>
+    </div>
+</div>
+
+<style>
+    .text {
+        font-family: 'Karla', sans-serif;
+        font-size: 40px;
+    }
+
+    table {
+        font-family: 'Karla', sans-serif;
+        font-size: 20px;
+    }
+</style>
---
+++ src/routes/TransactionInfo.svelte
@@ -0,0 +1,89 @@
+<script>
+    import BtnBack from "$lib/components/BtnBack.svelte";
+    import NavbarSolo from "$lib/components/NavbarSolo.svelte";
+    import Discard from "$lib/components/Discard.svelte";
+</script>
+
+<svelte:head>
+    <link href="https://fonts.googleapis.com/css2?family=Karla:wght@600&display=swap" rel="stylesheet"/>
+</svelte:head>
+
+<NavbarSolo />
+
+<div class="container">
+    <div class="columns  pt-5 is-multiline has-text-centered">
+        <div class="column is-4"> 
+            <a href="Customers"> <BtnBack /> </a> 
+        </div>
+        <div class="column is-4  ml-4">
+            <p class="text has-text-link">
+                0001
+            </p>
+            <p class="text has-text-link">
+                Transaction ID
+            </p>
+        </div>
+        <div class="column is-3">
+            <a href="/"> <Discard /> </a>
+        </div>
+    </div>
+
+    <div class="columns pt-5 is-multiline">
+        <div class="column is-1"></div>
+        <div class="column is-4">
+            <p class="text has-text-link">
+                Name
+            </p>
+            <p class="text has-text-black">
+                Bantugan, Dea
+            </p>
+        </div>
+        <div class="column is-3"></div>
+        <div class="column is-4">
+            <p class="text has-text-link">
+                Transaction Date
+            </p>
+            <p class="text has-text-black">
+                10/15/2022
+            </p>
+        </div>
+        <div class="column is-0"></div>
+        <div class="column is-2">
+            <p class="text has-text-link">
+                Type
+            </p>
+            <p class="text has-text-black">
+                Order
+            </p>
+        </div>
+        <div class="column is-2"></div>
+        <div class="column is-4">
+            <p class="text has-text-link">
+                Description
+            </p>
+            <p class="text has-text-black">
+                buy rice and adobo
+            </p>
+        </div>
+        <div class="column is-1"></div>
+        <div class="column is-2">
+            <p class="text has-text-link">
+                Amount
+            </p>
+            <p class="text has-text-black">
+                40.00
+            </p>
+        </div>
+    </div>
+</div>
+
+<style>
+    .text {
+        font-family: 'Karla', sans-serif;
+        font-size: 40px;
+    }
+    p {
+        font-family: 'Karla', sans-serif;
+        font-size: 40px;
+    }
+</style>
