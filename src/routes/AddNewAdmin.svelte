a67b5f369bb633538a9ae648145ee59b368c4303
brmangulabnan <brmangulabnan.edu@gmail.com>
2022-03-14 21:44:37 +0800

    [ADD] (T-441) / Added Add New Admin page

---
+++ src/routes/AddNewAdmin.svelte
@@ -0,0 +1,56 @@
+<script>
+    import Discard from "$lib/components/Discard.svelte";
+    import FieldWithValue from "$lib/components/FieldWithValue.svelte";
+    import NavbarSolo from "$lib/components/NavbarSolo.svelte";
+    import ButtonBack from "$lib/components/ButtonBack.svelte";
+    import ButtonAddRecord from "$lib/components/ButtonAddRecord.svelte";
+</script>
+
+<NavbarSolo/>
+
+<div class="container">
+    <div class="columns  pt-5 is-multiline has-text-centered">
+        <div class="column is-4">
+            <a href="Admin">
+                <ButtonBack/>
+            </a>
+        </div>
+        <div class="column is-4">
+            <p class="text has-text-link">
+                New Administrator
+            </p>
+        </div>
+        <div class="column is-3 ml-6">
+            <a href="Admin">
+                <Discard/>
+            </a>
+        </div>
+
+        <div class="column is-12"></div>
+        <div class="column is-12"></div>
+        <div class="column is-12"></div>
+        <FieldWithValue name="Name" value=""/>
+        <FieldWithValue name="Contact No." value=""/>
+        <div class="column is-12"></div>
+        <FieldWithValue name="Email" value=""/>
+        <FieldWithValue name="Password" value=""/>
+        <div class="column is-12"></div>
+        <div class="column is-12"></div>
+        <div class="column is-12"></div>
+        <div class="column is-12"></div>
+    </div>
+
+    <!-- Add record button -->
+    <div class="mb- has-text-centered">
+        <a href="Admin">
+            <ButtonAddRecord/>
+        </a>
+    </div>
+</div>
+
+<style>
+    .text {
+        font-family: 'Karla', sans-serif;
+        font-size: 40px;
+    }
+</style>
