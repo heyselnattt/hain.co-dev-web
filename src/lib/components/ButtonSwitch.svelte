b430c6153911ffc39724f4f7e895063045f2dedf
Hazel Anne Mejias <hazelannemejias1@gmail.com>
2022-03-19 16:26:56 +0800

    [REMOVED] / deleted files for it is refactored

--- src/lib/components/ButtonSwitch.svelte
+++
@@ -1,9 +0,0 @@
-<script lang="ts">
-    let isOn: boolean = false
-</script>
-
-<div class="field">
-    <input id="switchLarge switchColorDefault switchRoundedDefault" type="checkbox"
-           name="switchLarge switchColorDefault switchRoundedDefault" class="switch is-large is-link is-rounded">
-    <label for="switchLarge switchColorDefault switchRoundedDefault"></label>
-</div>
