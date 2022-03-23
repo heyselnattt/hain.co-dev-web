9679c493d8141ac8021d04472a22ae76163ac02b
Hazel Anne Mejias <hazelannemejias1@gmail.com>
2022-03-23 15:53:45 +0800

    [ADD] (Issue #32) / Added components for errormsg

---
+++ src/lib/components/otherComponents/ErrorMessage.svelte
@@ -0,0 +1,18 @@
+<script>
+    export let message;
+    let close = false;
+</script>
+
+<div class="notification is-danger {close ? "close" : ""}">
+    <button on:click={()=>{close=true}} class="delete"></button>
+    {message}
+</div>
+
+<style>
+    .notification{
+        min-height: 50px;
+    }
+    .close {
+        display: none;
+    }
+</style>
---
+++ src/lib/components/otherComponents/ErrorMessageContainer.svelte
@@ -0,0 +1,12 @@
+<div class="block">
+    <slot/>
+</div>
+
+<style>
+    .block {
+        min-width: 400px;
+        position: absolute;
+        top: 40px;
+        right: 60px;
+    }
+</style>
