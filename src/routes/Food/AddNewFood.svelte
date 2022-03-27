7a3e6ce3509afaa485d2161be9011035a8a3751e
brmangulabnan <brmangulabnan.edu@gmail.com>
2022-03-27 13:27:49 +0800

    [REFACTOR] (#31) / Var handler for selected image

--- src/routes/Food/AddNewFood.svelte
+++ src/routes/Food/AddNewFood.svelte
@@ -4,6 +4,17 @@
     import ButtonAddRecord from "$lib/components/buttons/ButtonAddRecord.svelte";
     import Discard from "$lib/components/buttons/Discard.svelte";
     import FieldWithValue from "$lib/components/otherComponents/FieldWithValue.svelte";
+
+    let  avatar, fileinput;
+	
+	const onFileSelected = (e) => {
+    let image = e.target.files[0];
+            let reader = new FileReader();
+            reader.readAsDataURL(image);
+            reader.onload = e => {
+                 avatar = e.target.result
+            };
+  }
 </script>
 
 <NavbarSolo/>
@@ -26,37 +37,41 @@
     <div class="columns pt-5 is-multiline">
         <div class="column is-12"></div>
         <FieldWithValue name="Product Name" value=""/>
-        <div class="column is-3"></div>
-        <div class="column is-3 control py-3">
+        <div class="column is-2"></div>
+        <div class="column is-3 control">
             <p class="has-text-link"></p>
             <textarea class="textarea has-fixed-size" placeholder="Description" ></textarea>
         </div>
         <FieldWithValue name="Price" value=""/>
-        <div class="column is-3"></div>
-        <div class="file">
+        <div class="column is-2"></div>
+
+        <div class="column is-3 file has-name">
+            {#if avatar}
+            <img class="avatar" src="{avatar}" alt="d" />
+            {:else}
+            <img class="avatar" src="../static/images/imageUpload.svg" alt="" /> 
+            {/if}
+
+            <input style="display:none" type="file" accept=".jpg, .jpeg, .png" on:change={(e)=>onFileSelected(e)} bind:this={fileinput} >
             <label class="file-label">
               <input class="file-input" type="file" name="resume">
               <span class="file-cta">
                 <span class="file-icon">
                   <i class="fas fa-upload"></i>
                 </span>
-                <span class="file-label">
+                <span class="upload" on:click={()=>{fileinput.click();}}>
                   Choose a file…
                 </span>
               </span>
             </label>
           </div>
         <div class="column is-12"></div>
-        
-        <div class="column is-12"></div>
-        <div class="column is-12"></div>
-        <div class="column is-12"></div>
 
         <!-- wala pang dropdown for product type, and file upload para sa image-->
     </div>
 
-    <div class="mb- has-text-centered">
-        <ButtonAddRecord link="Food"/>
+    <div class="mb-6 has-text-centered">
+        <ButtonAddRecord link="../Food"/>
     </div>
 </div>
 
@@ -70,4 +85,14 @@
         font-family: 'Karla', sans-serif;
         font-size: 40px;
     }
+    .upload {
+		display:flex;
+		cursor:pointer;
+	}
+	.avatar {
+		display:flex;
+		height:150px;
+		width:150px;
+        margin-bottom: 1.5rem;
+	}
 </style>
