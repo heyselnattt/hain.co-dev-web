d40409b89fa834d17fe2996420a5a6558f65a992
brmangulabnan <brmangulabnan.edu@gmail.com>
2022-04-22 01:09:32 +0800

    [REFACTOR] / Replaced FoodInfo w dynamic route

--- src/lib/components/tableRows/FoodTableRow.svelte
+++ src/lib/components/tableRows/FoodTableRow.svelte
@@ -1,17 +1,21 @@
 <script lang="ts">
+    import ButtonEditTable from "../buttons/ButtonEditTable.svelte";
+
     export let num;
     export let productName;
     export let price;
     export let type;
+    export let link: string;
 </script>
 
 <tbody>
-<tr class="text is-clickable">
-    <th>{num}</th>
-    <td>{productName}</td>
-    <td>{price}</td>
-    <td>{type}</td>
-</tr>
+    <tr class="text is-clickable">
+        <th>{num}</th>
+        <td>{productName}</td>
+        <td>{price}</td>
+        <td>{type}</td>
+        <td><ButtonEditTable link={link}/></td>
+    </tr>
 </tbody>
 
 <style>
--- src/routes/Food.svelte
+++ src/routes/Food.svelte
@@ -1,16 +1,38 @@
+<script context="module">
+    export async function load({ fetch }) {
+        const res = await fetch('https://jsonplaceholder.typicode.com/users')
+        const foods = await res.json()
+
+        if (res.ok) {
+            return {
+                props: {
+                    foods
+                }
+            }
+        }
+
+        return {
+            status: res.status,
+            error: new Error('Could not fetch the foods.')
+        }
+    }
+</script>
+
 <script>
     import NavbarWithSearch from "$lib/components/navbars/NavbarWithSearch.svelte";
     import ButtonBack from "$lib/components/buttons/ButtonBack.svelte";
     import ButtonAddRecord from "$lib/components/buttons/ButtonAddRecord.svelte";
     import FoodTableRow from "$lib/components/tableRows/FoodTableRow.svelte";
+
+    export let foods;
 </script>
 
-<NavbarWithSearch/>
+<NavbarWithSearch />
 
 <div class="container">
     <div class="columns has-text-centered pt-5">
         <div class="column is-4">
-            <ButtonBack link="Database"/>
+            <ButtonBack link="Database" />
         </div>
         <div class="column is-4">
             <p class="text has-text-link">
@@ -18,21 +40,22 @@
             </p>
         </div>
         <div class="column is-3 ml-6">
-                <ButtonAddRecord link="Food/AddNewFood"/>
+                <ButtonAddRecord link="Food/AddNewFood" />
         </div>
     </div>
 
     <div class="column is-10 is-offset-1 pl-5 pt-0">
         <table class="table is-hoverable is-fullwidth">
             <thead>
-            <tr>
-                <th>No.</th>
-                <th>Product Name</th>
-                <th>Price</th>
-                <th>Type</th>
-            </tr>
+                <tr>
+                    <th>No.</th>
+                    <th>Product Name</th>
+                    <th>Price</th>
+                    <th>Type</th>
+                    <th></th>
+                </tr>
             </thead>
-            <FoodTableRow num="1" productName="Hotdog" price="30.00" type="Breakfast"/>
+            <!-- <FoodTableRow num="1" productName="Hotdog" price="30.00" type="Breakfast"/>
             <FoodTableRow num="2" productName="Egg" price="25.00" type="Breakfast"/>
             <FoodTableRow num="3" productName="Adobo" price="30.00" type="Lunch"/>
             <FoodTableRow num="4" productName="Turon" price="35.00" type="Meryenda"/>
@@ -40,7 +63,22 @@
             <FoodTableRow num="6" productName="Menudo" price="40.00" type="Lunch"/>
             <FoodTableRow num="7" productName="RC" price="50.00" type="Drinks"/>
             <FoodTableRow num="8" productName="C2" price="20.00" type="Drinks"/>
-            <FoodTableRow num="9" productName="Banana Cue" price="20.00" type="Meryenda"/>
+            <FoodTableRow num="9" productName="Banana Cue" price="20.00" type="Meryenda"/> -->
+
+            {#each foods as food}
+                <FoodTableRow
+                    num={food.id}
+                    productName={food.name}
+                    price={food.phone}
+                    type={food.username}
+                    link={`/Food/${food.id}`}
+                />
+            {/each}
+
+            <!-- Temporary placeholders:
+                    Product name - name
+                    Price - phone
+                    Type - username -->
         </table>
     </div>
 </div>
