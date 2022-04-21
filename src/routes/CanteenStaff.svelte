2eec70c7345fbf3c237d9a111c3741cc175a4ba1
brmangulabnan <brmangulabnan.edu@gmail.com>
2022-04-22 01:09:08 +0800

    [REFACTOR] / Replaced Staff Info w dynamic route

--- src/routes/CanteenStaff.svelte
+++ src/routes/CanteenStaff.svelte
@@ -1,15 +1,33 @@
+<script context="module">
+    export async function load({ fetch }) {
+        const res = await fetch('https://jsonplaceholder.typicode.com/users')
+        const canteenStaffs = await res.json()
+
+        if (res.ok) {
+            return {
+                props: {
+                    canteenStaffs
+                }
+            }
+        }
+
+        return {
+            status: res.status,
+            error: new Error('Could not fetch the cateen staffs.')
+        }
+    }
+</script>
+
 <script lang="ts">
     import NavbarWithSearch from "$lib/components/navbars/NavbarWithSearch.svelte";
     import ButtonBack from "$lib/components/buttons/ButtonBack.svelte";
     import ButtonAddRecord from "$lib/components/buttons/ButtonAddRecord.svelte";
     import CanteenStaffTableRow from "$lib/components/tableRows/CanteenStaffTableRow.svelte";
+
+    export let canteenStaffs;
 </script>
 
-<svelte:head>
-    <link href="https://fonts.googleapis.com/css2?family=Karla:wght@600&display=swap" rel="stylesheet"/>
-</svelte:head>
-
-<NavbarWithSearch/>
+<NavbarWithSearch />
 
 <div class="container">
     <div class="columns has-text-centered pt-5">
@@ -22,22 +40,24 @@
             </p>
         </div>
         <div class="column is-3 ml-6">
-            <ButtonAddRecord link="CanteenStaff/AddNewCanteenStaff"/>
+            <ButtonAddRecord link="CanteenStaff/AddNewCanteenStaff" />
         </div>
     </div>
 
     <div class="column is-10 is-offset-1 pl-5 pt-0">
         <table class="table is-hoverable is-fullwidth">
             <thead>
-            <tr>
-                <th>No.</th>
-                <th>Name</th>
-                <th>Position</th>
-                <th>Contact No.</th>
-                <th>Address</th>
-            </tr>
+                <tr>
+                    <th>No.</th>
+                    <th>Name</th>
+                    <th>Position</th>
+                    <th>Contact No.</th>
+                    <th>Address</th>
+                    <th></th>
+                </tr>
             </thead>
-            <CanteenStaffTableRow num="1" name="Salaveria, Rence" position="Cook" contactNum="09123456789"
+
+            <!-- <CanteenStaffTableRow num="1" name="Salaveria, Rence" position="Cook" contactNum="09123456789"
                                   address="02 Toronto St. Malinta, Valenzuela City"/>
             <CanteenStaffTableRow num="2" name="Delica, Lean O." position="Cook" contactNum="09523648951"
                                   address="211 Maple St. Pateros, Metro Manila"/>
@@ -52,7 +72,22 @@
             <CanteenStaffTableRow num="7" name="Roche, Razelle" position="Server" contactNum="09055964325"
                                   address="78 Maysan Rd. Maysan, Valenzuela City"/>
             <CanteenStaffTableRow num="8" name="Lee, Mark" position="Server" contactNum="09994563221"
-                                  address="024 Buenavista St. Malinta, Valenzuela City"/>
+                                  address="024 Buenavista St. Malinta, Valenzuela City"/> -->
+
+            {#each canteenStaffs as canteenStaff}
+                <CanteenStaffTableRow
+                    num={canteenStaff.id}
+                    name={canteenStaff.name}
+                    position={canteenStaff.username}
+                    contactNum={canteenStaff.phone}
+                    address={canteenStaff.email}
+                    link={`/CanteenStaff/${canteenStaff.id}`}
+                />
+            {/each}
+
+            <!-- Temporary placeholders:
+                    Position - username
+                    Address - email -->
         </table>
     </div>
 </div>
