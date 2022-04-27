274740fb82cc7e2a2f9245e0f3f6da299741b767
brmangulabnan <brmangulabnan.edu@gmail.com>
2022-04-28 00:22:27 +0800

    [ADD] Slug for Admin

---
+++ src/routes/Admin/[username].svelte
@@ -0,0 +1,79 @@
+<script context="module">
+    import axios from "$lib/api/index"
+    export async function load({ fetch, params }) {
+        try {
+            const username = params.username;
+            const admin = await axios.get(`/admin/${username}`);
+            console.log(admin)
+            return {
+                props: {
+                    admin
+                }
+            }
+        } catch (e) {
+            return {
+                error: new Error('Can\'t fetch the admin')
+            }
+        }
+    }
+</script>
+
+<script>
+    import ButtonBack from "$lib/components/buttons/ButtonBack.svelte";
+    import ButtonSave from "$lib/components/buttons/ButtonSave.svelte";
+    import FieldWithValue from "$lib/components/otherComponents/FieldWithValue.svelte";
+    import NavbarSolo from "$lib/components/navbars/NavbarSolo.svelte";
+
+    export let admin;
+</script>
+
+<NavbarSolo/>
+
+<div class="container">
+    <div class="columns  pt-5 is-multiline has-text-centered">
+        <div class="column is-4">
+            <ButtonBack link="../Admin"/>
+        </div>
+        <div class="column is-4">
+            <p class="text has-text-link">
+                Admin's Information
+            </p>
+        </div>
+        <div class="column is-3 ml-6">
+            <ButtonSave link="../Admin"/>
+        </div>
+    </div>
+
+    <div class="columns pt-5 is-multiline">
+        <div class="column is-12"></div>
+        {#await admin}
+            Waiting data
+        {:then admin}
+            <!-- TODO switch to bound inputs -->
+            <!-- TODO: yung is active pa na checkbox ilagay -->
+            <FieldWithValue 
+                name="Name"
+                value={admin.data.admin_full_name}/>
+            <FieldWithValue
+                name="Username"
+                value={admin.data.admin_username}/>
+            <FieldWithValue
+                name="Activity"
+                value={admin.data.admin_is_active}/>
+            <FieldWithValue 
+                name="Password"
+                value={admin.data.admin_password}/>
+            {:catch e} 
+                {e}
+            {/await}
+
+        <div class="column is-12"></div>
+    </div>
+</div>
+
+<style>
+    .text {
+        font-family: 'Karla', sans-serif;
+        font-size: 40px;
+    }
+</style>
