78767a60e8945bbbcf646b6f1c7bce72fee89600
brmangulabnan <brmangulabnan.edu@gmail.com>
2022-05-25 16:12:48 +0800

    [CONTENT] / Changed table contents

---
+++ src/lib/components/tableRows/OrdersTableRow.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+    export let productCode;
+    export let customerEmail;
+    export let orderRequest;
+    export let orderDate;
+    export let staffUsername;
+    export let orderStatus;
+    export let orderNumber;
+</script>
+
+<tbody>
+    <tr class="text is-clickable">
+        <th>{productCode}</th>
+        <td>{customerEmail}</td>
+        <td>{orderRequest}</td>
+        <td>{orderDate}</td>
+        <td>{staffUsername}</td>
+        <td>{orderStatus}</td>
+        <td>{orderNumber}</td>
+    </tr>
+</tbody>
+
+<style>
+    .text {
+        font-size: 17px;
+    }
+</style>
