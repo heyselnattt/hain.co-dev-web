2303bb7cc537a3d57b979eb9e23a8ee09b77021b
Rence Salaveria <clarencerheysalaveria@plv.edu.ph>
2022-05-26 00:13:47 +0800

    [IMPROVEMENT] / Edited the TableRow to reflect data shape

--- src/lib/components/tableRows/TransactionTableRow.svelte
+++ src/lib/components/tableRows/TransactionTableRow.svelte
@@ -1,18 +1,20 @@
 <script lang="ts">
-    export let num;
-    export let name;
-    export let desc;
-    export let amount;
+    export let id;
+    export let agent;
+    export let description;
     export let type;
+    export let amount;
+    export let date;
 </script>
 
 <tbody>
     <tr class="text is-clickable" >
-        <th>{num}</th>
-        <td>{name}</td>
-        <td>{desc}</td>
+        <th>{id}</th>
+        <td>{agent}</td>
+        <td>{description}</td>
         <td>{amount}</td>
         <td>{type}</td>
+        <td>{date}</td>
     </tr>
 </tbody>
 
