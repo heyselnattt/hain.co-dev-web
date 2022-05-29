180db09ba691bb9a44b68576d96eb10f97e449b2
Rence Salaveria <clarencerheysalaveria@plv.edu.ph>
2022-05-30 01:39:17 +0800

    [IMPROVEMENT] / Update product now functional

--- src/routes/Food/[code].svelte
+++ src/routes/Food/[code].svelte
@@ -5,10 +5,12 @@
         try {
             const productCode = params.code;
             const product = await axios.get(`/product/${productCode}`);
+            const oldProductCode = product.data.product_code;
             console.log(product)
             return {
                 props: {
-                    product
+                    product,
+                    oldProductCode
                 }
             }
         } catch (e) {
@@ -22,11 +24,7 @@
 <script>
     import NavbarSolo from "$lib/components/navbars/NavbarSolo.svelte";
     import ButtonBack from "$lib/components/buttons/ButtonBack.svelte";
-    import FieldWithValue from "$lib/components/otherComponents/FieldWithValue.svelte";
-    import ButtonSave from "$lib/components/buttons/ButtonSave.svelte";
-    import ButtonSwitch from "$lib/components/buttons/ButtonSwitch.svelte";
-    import Dropdown from "$lib/components/otherComponents/Dropdown.svelte";
-    import Food from "../Food.svelte";
+    import {goto} from "$app/navigation";
 
     const identifyType = (code) => {
         switch (code) {
@@ -45,7 +43,40 @@
     }
 
     export let product;
+    export let oldProductCode;
+
+    let newProduct = {
+        product_name: null,
+        product_price: null,
+        product_image_link: null,
+        product_stock: null,
+        product_description: null,
+        product_type: null,
+        product_is_active: true,
+        product_code: null
+    }
+
+    let types = [
+        {value: 1, position: "BREAKFAST"},
+        {value: 2, position: "LUNCH"},
+        {value: 3, position: "DESSERT"},
+        {value: 4, position: "EXTRA"}
+    ]
+
+    const updateProductToDatabase = async () => {
+        try {
+            let response = await axios.put(`/product/update_product/${oldProductCode}`, newProduct)
+            console.log(response)
+            await goto('../Food');
+        } catch (e) {
+            console.log(e);
+        }
+    }
 </script>
+
+<svelte:head>
+    <link href="https://fonts.googleapis.com/css2?family=Karla:wght@600&display=swap" rel="stylesheet"/>
+</svelte:head>
 
 <NavbarSolo/>
 
@@ -60,59 +91,111 @@
             </p>
         </div>
         <div class="column is-3 ml-6">
-            <ButtonSave link="../Food"/>
+            <button class="button is-rounded is-info btn-txt" on:click={updateProductToDatabase}>
+                <p class="ml-4 mr-4 has-text-white">
+                    Save
+                </p>
+            </button>
         </div>
     </div>
 
     <div class="columns pt-5 is-multiline">
-        <div class="column is-12"></div>
+        <div class="column is-12">
+            <div class="has-text-centered pText">Make sure to fill up all information boxes</div>
+        </div>
         {#await product}
             Waiting data
         {:then food}
-            <!-- TODO switch to bound inputs -->
-            <FieldWithValue
-                name="Product Name"
-                value={food.data.product_name}/>
-            <FieldWithValue
-                name="Price"
-                value={food.data.product_price}/>
-            <FieldWithValue
-                name="Stock"
-                value={food.data.product_stock}/>
-            <Dropdown
-                name="Product Type"
-                type={food.data.product_type}/>
-            <FieldWithValue
-                name="Product Description"
-                value={food.data.product_description}/>
-            <FieldWithValue
-                name="Product Code"
-                value={food.data.product_code}/>
-            <FieldWithValue
-                name="Food image Link"
-                value={food.data.product_image_link}/>
-            <div class="column is-3 is-offset-2 pt-5 pl-4 has-text-link">
-                <ButtonSwitch
-                    isOn={food.data.product_is_active}/>
+
+            <div class="column is-3 is-offset-2">
+                <p class="pText has-text-link ml-4 mb-1">
+                    Current Name: {food.data.product_name}
+                </p>
+                <input class="pText input is-rounded" type="text" bind:value={newProduct.product_name}/>
+            </div>
+            <!-- TODO number validation -->
+            <div class="column is-3 is-offset-2">
+                <p class="pText has-text-link ml-4 mb-1">
+                    Current Price: {food.data.product_price}
+                </p>
+                <input class="pText input is-rounded" type="number" bind:value={newProduct.product_price}/>
+            </div>
+            <!-- TODO number validation -->
+            <div class="column is-3 is-offset-2">
+                <p class="pText has-text-link ml-4 mb-1">
+                    Current Stock: {food.data.product_stock}
+                </p>
+                <input class="pText input is-rounded" type="number" bind:value={newProduct.product_stock}/>
+            </div>
+            <div class="column is-3 is-offset-2">
+                <p class="pText has-text-link ml-4 mb-1">
+                    Current Type: {identifyType(food.data.product_type)}
+                </p>
+                <select bind:value={newProduct.product_type} class="pText input is-rounded">
+                    {#each types as pos}
+                        <option value={pos.value}>
+                            {pos.position}
+                        </option>
+                    {/each}
+                </select>
+            </div>
+            <!-- TODO make sure unique -->
+            <div class="column is-3 is-offset-2">
+                <p class="pText has-text-link ml-4 mb-1">
+                    Current Code: {food.data.product_code}
+                </p>
+                <input class="pText input is-rounded" type="text" bind:value={newProduct.product_code}/>
+            </div>
+            <div class="column is-3 is-offset-2">
+                <p class="pText has-text-link ml-4 mb-1">
+                    Current Link: {food.data.product_image_link}
+                </p>
+                <input class="pText input is-rounded" type="text" bind:value={newProduct.product_image_link}/>
+            </div>
+            <div class="column is-3 is-offset-2">
+                <p class="pText has-text-link ml-4 mb-1">
+                    Current Description: {food.data.product_description}
+                </p>
+                <textarea class="pText input tall-textarea" bind:value={newProduct.product_description}></textarea>
             </div>
             <div class="column is-3 is-offset-2">
                 <div class="pText has-text-link ml-4 mb-1">
                     <p>Image Preview</p>
-                    <figure class="avatar mt-2">
-                        <img src="{food.data.product_image_link}" alt=""/>
+                    <figure class="mt-4 avatar food-image">
+                        {#if isNaN(newProduct.product_image_link)}
+                            <img src="{newProduct.product_image_link}" alt="" style="border-radius: 20px; border: hsl(223, 85%, 41%)  5px double"/>
+                        {/if}
                     </figure>
                 </div>
             </div>
 
             <div class="column is-12"></div>
-            <div class="column is-12"></div>
+
+            <div class="column is-12 has-text-centered">
+                <div class="field">
+                    {#if food.data.product_is_active}
+                        <div class="pText has-text-centered">Product Currently Active</div>
+                    {:else}
+                        <div class="pText has-text-centered">Product Currently Inactive</div>
+                    {/if}
+                    <input id="switchLarge switchColorDefault switchRoundedDefault"
+                           type="checkbox"
+                           name="switchLarge switchColorDefault switchRoundedDefault"
+                           class="switch is-large is-link is-rounded"
+                           bind:checked={newProduct.product_is_active}>
+                    {#if newProduct.product_is_active}
+                        <label class="pText" for="switchLarge switchColorDefault switchRoundedDefault">Active</label>
+                    {:else}
+                        <label class="pText" for="switchLarge switchColorDefault switchRoundedDefault">Inactive</label>
+                    {/if}
+                </div>
+            </div>
         {:catch e}
             {e}
         {/await}
-
-        <!-- wala pang file upload for image and also yung dropdown para sa product type -->
     </div>
 </div>
+
 
 <style>
     .text {
@@ -127,9 +210,19 @@
 
     .avatar {
         display: flex;
-        height: 150px;
-        width: 150px;
-        margin-bottom: 1.5rem;
+        aspect-ratio: 4 / 5 auto;
+        max-width: 250px;
+        margin-top: 1.5rem;
+    }
+
+    .btn-txt {
+        font-size: 20px;
+        font-family: 'Karla', sans-serif;
+    }
+
+    .tall-textarea {
+        height: 18rem;
+        border-radius: 20px;
     }
 </style>
 
