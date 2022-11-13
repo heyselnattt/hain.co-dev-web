b201d1f2683d49017f8adad1e0e2b2fe022d720e
Hazel Anne Mejias <hazelannemejias1@gmail.com>
2022-11-14 03:14:42 +0800

    db url

--- src/lib/api/index.ts
+++ src/lib/api/index.ts
@@ -3,6 +3,6 @@ import axios from 'axios';
 export default axios.create({
 	// baseURL:"https://hainco-api.herokuapp.com/",
 	// baseURL: "https://sb-hainco.herokuapp.com/api"
-	// baseURL: "https://hainco-sb.herokuapp.com/api",
-	baseURL: "http://localhost:8092/api",
+	baseURL: "https://hainco-sb.herokuapp.com/api",
+	// baseURL: "http://localhost:8092/api",
 });
